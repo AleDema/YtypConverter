7c8ea2ea2447bcfe002f1abd42cfddef32103080
FROST-D <ale.morelli_2@libero.it>
2020-03-14 18:04:47 +0100

    -

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import org.apache.commons.math3.complex.Quaternion;
-
+import org.apache.commons.math3.complex.*;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import org.w3c.dom.Document;
@@ -19,7 +18,7 @@ public class Main {
 		//String s = "CreateObject(GetHashKey(" + hash + "),generator.x-1.9,generator.y+3.0,generator.z+0.38,false,false,false)";
 		
 		try {
-	         File inputFile = new File("apa_test.xml");
+	         File inputFile = new File("input.xml");
 	         StringBuilder sb = new StringBuilder();
 	         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 	         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -38,7 +37,7 @@ public class Main {
 	        		 NodeList testdio = eElement.getElementsByTagName("name");
 	        		 Element name = (Element) testdio.item(0);
 	        		 String s = name.getTextContent();
-	        		 if (s.equals("apa_V_mp_h_01")){
+	        		 if (s.equals("v_apartment_high")){
 	        			// nList = (NodeList) nList.item(temp);
 	        			 index = temp;
 	        			 System.out.println("test");
@@ -62,122 +61,56 @@ public class Main {
 	          		  Element eElement2 = (Element) nNode2;
 		              String test = eElement2.getAttribute("type");
 		          //	  System.out.println(test);
-		          	  
-		          	  
+
 		          	 if ( test.equals("CEntityDef")){
-			            
-			            	
-			            	 NodeList testdio = eElement2.getElementsByTagName("position");
-			            	 NodeList rotList = eElement2.getElementsByTagName("rotation");
-			            	 NodeList diocan = eElement2.getElementsByTagName("archetypeName");
-			            	 Element position = (Element) testdio.item(0);
-			            	 Element rotation = (Element) rotList.item(0);
-			            	 Element archetype = (Element) diocan.item(0);
-			            	 String hash = '`' + archetype.getTextContent() + '`';
-			            	 
-			            	// if (! hash.contains("V_16")) {
-			            	 String x =  position.getAttribute("x");
-			            	 String y =  position.getAttribute("y");
-			            	 String z =  position.getAttribute("z");
-			            	 
-			            	 float rotX = Float.parseFloat( rotation.getAttribute("x"));
-			            	 float rotY = Float.parseFloat( rotation.getAttribute("y"));
-			            	 float rotZ = Float.parseFloat( rotation.getAttribute("z"));
-			            	 float rotW = Float.parseFloat( rotation.getAttribute("w"));
-			            	 
-			            	 
-			            	 /* Not working
-			            	    // roll (x-axis rotation)
-			            	    double sinr_cosp = 2 * (rotW * rotX + rotY * rotZ);
-			            	    double cosr_cosp = 1 - 2 * (rotX* rotX + rotY * rotY);
-			            	   double roll = Math.atan2(sinr_cosp, cosr_cosp);
-			            	   double pitch = 0;
-			            	   double yaw = 0;
+						 NodeList testdio = eElement2.getElementsByTagName("position");
+						 NodeList rotList = eElement2.getElementsByTagName("rotation");
+						 NodeList diocan = eElement2.getElementsByTagName("archetypeName");
+						 Element position = (Element) testdio.item(0);
+						 Element rotation = (Element) rotList.item(0);
+						 Element archetype = (Element) diocan.item(0);
+						 String hash = '`' + archetype.getTextContent() + '`';
 
+						// if (! hash.contains("V_16")) {
+						 String x =  position.getAttribute("x");
+						 String y =  position.getAttribute("y");
+						 String z =  position.getAttribute("z");
 
-			            	    // pitch (y-axis rotation)
-			            	    double sinp = 2 * (rotW * rotY - rotZ * rotZ);
-			            	    if (Math.abs(sinp) >= 1){
-			            	    	System.out.println("dio");
-			            	    	pitch = Math.copySign(Math.PI / 2, sinp); // use 90 degrees if out of range
-			            	    }
-			            	    //  use 90 degrees if out of range
-			            	    else
-			            	        pitch = Math.asin(sinp);
+						 float rotX = Float.parseFloat( rotation.getAttribute("x"));
+						 float rotY = Float.parseFloat( rotation.getAttribute("y"));
+						 float rotZ = Float.parseFloat( rotation.getAttribute("z"));
+						 float rotW = Float.parseFloat( rotation.getAttribute("w"));
 
-			            	    // yaw (z-axis rotation)
-			            	    double siny_cosp = 2 * (rotW * rotZ + rotX * rotY);
-			            	    double cosy_cosp = 1 - 2 * (rotY * rotY + rotZ * rotZ);
-			            	    yaw = Math.atan2(siny_cosp, cosy_cosp);
-			            	    
-			            	    
-			            	    //How it should be
-								public void SetOrientation(Quaternion ori, bool inverse = false)
-								        {
-								            if (MloParent != null)
-								            {
-								                var mloInv = Quaternion.Normalize(Quaternion.Invert(MloParent.Orientation));
-								                Quaternion rel = Quaternion.Normalize(Quaternion.Multiply(mloInv, ori));
-								                Quaternion inv = Quaternion.Normalize(Quaternion.Invert(rel));
-								                Orientation = ori;
-								                _CEntityDef.rotation = inv.ToVector4();
-								            }
-								            else
-								            {
-								                Orientation = inverse ? Quaternion.Normalize(Quaternion.Invert(ori)) : ori;
-								                if (MloInstance != null)
-								                {
-								                    _CEntityDef.rotation = Orientation.ToVector4();
-								                }
-								                else
-								                {
-								                    Quaternion inv = inverse ? ori : Quaternion.Normalize(Quaternion.Invert(ori));
-								                    _CEntityDef.rotation = inv.ToVector4();
-								                }
-								            }
-								
-								            if (MloInstance != null)
-								            {
-								                MloInstance.SetOrientation(ori);
-								                MloInstance.UpdateEntities();
-								            }
-								
-								            UpdateBB();
-								            UpdateWidgetPosition();
-								            UpdateWidgetOrientation();
-								        }
+						 Quaternion our =  new Quaternion(rotX,rotY,rotZ,rotW);
+						 Quaternion parent = new Quaternion(0,0,0,1);
+						 parent = parent.getInverse();
+						 parent = parent.normalize();
 
-			            	    
-			            	    */ 
-			            	 
-			            	    Quaternion q = new Quaternion(0.0, 0.0, 0.0, 1.0);
-			            	 
-			            	    
-			            	    System.out.println("test: " + q.getInverse());
-			            	 
-			            	 
-			            	 
-			            	 
-			            	 String item = "item";
-			            	 String create = item + " = CreateObject(" + hash + ",generator.x + " + x + " ,generator.y +" + y + ",generator.z+ " + z + ",false,false,false)\n";
-			            	 String quaternion = "SetEntityQuaternion(" + item + "," + rotX + "," + rotY + "," + rotZ + "," + rotW + ")\n";
-			            	 String freeze = "FreezeEntityPosition(" + item + ",true)\n";
-			            	 String insert = "table.insert(objects," + item + ")\n";
-			            	 
-			            	 sb.append(create);
-			            	 sb.append(quaternion);
-			            	 sb.append(freeze);
-			            	 sb.append(insert);
+						 Quaternion newQ = parent.multiply(our);
+						 newQ = newQ.normalize();
 
-			            }
-		          	  
+						 newQ = newQ.getInverse();
+						 newQ = newQ.normalize();
+
+						 System.out.println(hash);
+						 System.out.println("NEWQ");
+						 System.out.println(newQ);
+
+						 String item = "item";
+						 String create = item + " = CreateObject(" + hash + ",generator.x + " + x + " ,generator.y +" + y + ",generator.z+ " + z + ",false,false,false)\n";
+						 String quaternion = "SetEntityQuaternion(" + item + "," + rotX + "," + rotY + "," + rotZ + "," + rotW + ")\n";
+						 String freeze = "FreezeEntityPosition(" + item + ",true)\n";
+						 String insert = "table.insert(objects," + item + ")\n";
+			            	 
+						 sb.append(create);
+						 sb.append(quaternion);
+						 sb.append(freeze);
+						 sb.append(insert);
+
+		          	 }
 	          	  }
-	          	  
-
 	            }
-	            
 	         }
-	         
 	         File file = new File("file.txt");
 	         BufferedWriter writer = null;
 	         try {
